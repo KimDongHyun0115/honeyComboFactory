1c9a6fb9b33cce2e7f20e711e1ddc7719549e3bf
김동현 <donghyun960115@naver.com>
2025-05-20 15:57:58 +0900

    조회수 업데이트

--- src/main/java/zic/honeyComboFactory/biz/boardCombo/impl/OracleBoardComboDAOMybatis.java
+++ src/main/java/zic/honeyComboFactory/biz/boardCombo/impl/OracleBoardComboDAOMybatis.java
@@ -52,6 +52,7 @@ public class OracleBoardComboDAOMybatis { // 꿀조합 게시판 기능 - Oracle
 			System.out.println("게시글 수정");
 			result = mybatis.update("BoardComboDAO.UPDATECOMBOBOARD", boardComboVO);
 			if (result <= 0) {
+				System.out.println("게시글 수정 실패");
 				return false;
 			}
 		} 
@@ -60,6 +61,7 @@ public class OracleBoardComboDAOMybatis { // 꿀조합 게시판 기능 - Oracle
 			System.out.println("조회수 업데이트");
 			result = mybatis.update("BoardComboDAO.UPDATEVIEWCOUNT", boardComboVO);
 			if(result<=0) {
+				System.out.println("조회수 업데이트 실패");
 				return false;
 			}
 		}
